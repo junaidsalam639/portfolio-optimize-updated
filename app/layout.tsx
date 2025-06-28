a6f4cd4081538507af03f54d1eae73c8289a364c
junaidhnhtechsolutions <junaidhnhtechsolutions@gmail.com>
2025-06-28 14:38:50 +0500

    review script

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,23 +23,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js"
           strategy="lazyOnload"
         />
-        <Script
-          id="tawk-to"
-          strategy="lazyOnload"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/6239f19d2bd26d087e742876/1fup5fk6a';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-              })();
-            `,
-          }}
-        />
       </head>
       <body className={inter.className}>
         <ApolloClientProvider>
